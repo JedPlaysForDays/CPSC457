2c51793d83b5f3883b7fb852ac38e313aabb3352
30212116 <jed.cravalho@ucalgary.ca>
2025-09-15 22:35:06 -0600

    Started initial code (filename, fork, etc...)

--- Assignments/A1/a1p1.c
+++ Assignments/A1/a1p1.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main() {
-    s
+
+    pid_t fr;
+
+
+    printf("");
+    char filename[] = scanf(stdin);
+    FILE *file = fopen(filename, "r");
+    if (file != NULL) {
+        
+    }
+
+
+
+    for (int i = 0; i < 100; i++) {
+        fr = fork();
+        if (fr < 0) {
+            fprintf(stderr, "error\n");
+            exit(-1);
+        } else if (fr == 0) {
+        
+            
+        } else {
+            wait(NULL);
+            printf("Success!!\n");
+            exit(0);
+        }
+        
+    }
+    printf("Starting treasure hunt:\n");
+
+    
+
+    
 }
