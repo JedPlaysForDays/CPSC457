302988798dd6852bd854fd2fd68a5e5f1b5ddb62
30212116 <jed.cravalho@ucalgary.ca>
2025-09-15 23:17:11 -0600

    Testing fork

--- Assignments/A1/a1p1.c
+++ Assignments/A1/a1p1.c
@@ -5,35 +5,32 @@
 int main() {
 
     pid_t fr;
+    int toParent;
+    int toChild;
 
+    char filename[100];
+    printf("Enter the file to scan:\n");
+    fgets(filename, sizeof(filename), stdin);
 
-    printf("");
-    char filename[] = scanf(stdin);
+    // Open file
     FILE *file = fopen(filename, "r");
-    if (file != NULL) {
-        
+    if (file == NULL) {
+        printf("An error occured while reading the file\n");
     }
-
 
 
     for (int i = 0; i < 100; i++) {
         fr = fork();
         if (fr < 0) {
             fprintf(stderr, "error\n");
-            exit(-1);
         } else if (fr == 0) {
-        
-            
+            printf("I am child %d!\n", i);
+
         } else {
-            wait(NULL);
-            printf("Success!!\n");
-            exit(0);
+            printf("Child %d (PID %d): Searching row %d\n", i, fr, i);
         }
-        
     }
-    printf("Starting treasure hunt:\n");
 
-    
 
-    
+    return 0;
 }
