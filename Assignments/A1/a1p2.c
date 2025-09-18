19d994dc5ffc57cd7c751435ac9e77dbae4ac9e9
30212116 <jed.cravalho@ucalgary.ca>
2025-09-18 16:21:31 -0600

    Fixed race conditions and added functionality of checking primes

--- Assignments/A1/a1p2.c
+++ Assignments/A1/a1p2.c
@@ -33,10 +33,10 @@ int main(int argc, char *argv[]) {
     if (argc != 4) {
         printf("There must be 3 arguments in the format shown: ./a1p2 [UPPER_BOUND] [LOWER_BOUND] [NUM_PROCESSES]\n");
         exit(0);
-    } else if (argv[3] <= 0) {
+    } else if (atoi(argv[3]) <= 0) {
         printf("The number of processes must be greater than 0.\n");
         exit(0);
-    } else if (argv[2]-argv[1] <= 0) {
+    } else if (atoi(argv[2]) - atoi(argv[1]) <= 0) {
         printf("The difference between the bounds must be greater than 0\n");
         exit(0);
     } else {
@@ -51,7 +51,6 @@ int main(int argc, char *argv[]) {
     if (N > totalRange) { // Sets the highest number of children to the total range (can't have bigger range than children)
         N = totalRange;
     }
-
     // do ceiling function for max primes per child. This ensures that there is equal coverage for each process for the entire range of numbers.
     MAX_PRIMES_PER_CHILD = (totalRange + N - 1) / N;
 
@@ -63,27 +62,40 @@ int main(int argc, char *argv[]) {
     int *shm_ptr = (int *) shmat(shmid, NULL, 0); //pointer to shared memory 
 
     for (int i = 0; i < N; i++) {
+
         fr = fork();
         if (fr < 0) {
             fprintf(stderr, "There was an error with child PID: %d. Shutting down.", getpid());
             exit(-1);
         } else if (fr == 0) {
-        /* Child Process */
-        // Computes a non-overlapping subrange of the total range.
-        int index = shm_ptr[0]++;
-        int low = LOWER_BOUND + index * MAX_PRIMES_PER_CHILD;
-        int high = low + MAX_PRIMES_PER_CHILD - 1;
-        if (high > UPPER_BOUND) {
-            high = UPPER_BOUND;
-        }
-        printf("Child (PID: %d) Checking range [%d, %d]\n", getpid(), low, high);
-        // for (int j = low; j < high; j++) {
-        
-        // }
-        // Finds prime numbers in that subrange.
-        // Stores them in the shared memory in a thread-safe manner (e.g., using an offset scheme).
-        // Exits.
-        exit(0);
+            /* Child Process */
+            // Computes a non-overlapping subrange of the total range.
+
+            int base = 1 + i * MAX_PRIMES_PER_CHILD;
+            int count = 0;
+
+            int low = LOWER_BOUND + i * MAX_PRIMES_PER_CHILD;
+            int high = LOWER_BOUND + (i + 1) * MAX_PRIMES_PER_CHILD - 1;
+            if (high > UPPER_BOUND) {
+                high = UPPER_BOUND;
+            }
+
+            printf("Child PID %d checking range [%d, %d]\n", getpid(), low, high);
+            
+            for (int n = LOWER_BOUND; n <= UPPER_BOUND; n++) {
+                if (is_prime(n)) {
+                    shm_ptr[base + count] = n;
+                    count++;
+                }
+            }
+            shm_ptr[base + count] = -1; // sentinel to mark end
+            // for (int j = low; j < high; j++) {
+            
+            // }
+            // Finds prime numbers in that subrange.
+            // Stores them in the shared memory in a thread-safe manner (e.g., using an offset scheme).
+            // Exits.
+            exit(0);
         } else {
             
         }
@@ -100,4 +112,8 @@ int main(int argc, char *argv[]) {
     // read shared memory
     // print out all primes (hopefully in order?)
     // free up shared memory
+    shmdt(shm_ptr);
+    shmctl(shmid, IPC_RMID, NULL);
+
+    exit(0);
 }
