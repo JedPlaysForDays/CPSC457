0ebc6efcc27df740f45a92082046f57226a4c07f
30212116 <jed.cravalho@ucalgary.ca>
2025-09-18 19:12:51 -0600

    fixed test3 causing a segfault due to rowTreasure not initialized when no treaure

--- Assignments/A1/Assignment 1 - Inputs/a1p1.c
+++ Assignments/A1/Assignment 1 - Inputs/a1p1.c
@@ -13,7 +13,7 @@
 int main(int argc, char *argv[]) {
 
     pid_t fr;               // store child pid
-    int rowOfTreasure;      // store the row of the winning treasure
+    int rowOfTreasure = -1; // store the row of the winning treasure (-1 for no treasure found)
     int winnerPID;          // store winning child's PID   
     int rows = 100;         // set number of rows to 100 (since it's a 100x1000 matrix)
     int cols = 1000;        // set number of rows to 1000
@@ -29,8 +29,7 @@ int main(int argc, char *argv[]) {
             exit(1);
             fclose(file);
         } else {
-
-            /* Fill Matrix from test file*/
+            /* Fill Matrix from test file */
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < cols; j++) {
                     fscanf(file, "%d", &matrix[i][j]);
@@ -46,16 +45,15 @@ int main(int argc, char *argv[]) {
                     exit(-1);
 
                 } else if (fr == 0) { 
-
                     /* Child Process */
                     printf("Child %d (PID %d): Searching row %d\n", i, getpid(), i);
                     for (int j = 0; j < cols; j++) {
                         /* Find Treasure */
                         if (matrix[i][j] == 1) {
-                            exit(i+1); // +1 to avoid issues if treasure in row 0
+                            _exit(i+1); // +1 to avoid issues if treasure in row 0
                         }
                     }
-                    exit(0); // exit 0 if no treasure was found in row
+                    _exit(0); // exit 0 if no treasure was found in row
                 }
             }
             
@@ -63,25 +61,28 @@ int main(int argc, char *argv[]) {
             for (int j = 0; j < 100; j++) {
                 int status;
                 int childPID = wait(&status);
-                if (WIFEXITED(status) && WEXITSTATUS(status) > 0) { // if child process exits and succeeds finding the treasure
+                if (WEXITSTATUS(status) > 0) { // if child process exits and succeeds finding the treasure
                     rowOfTreasure = WEXITSTATUS(status) - 1; // -1 to fix offset from exit code, save winning row
                     winnerPID = childPID; // save winning child PID
                 }
-            }                        
-            for (int j = 0; j < 1000; j++) { // Find column of treasure given winning row
+            }
+            // Error-checking if no treasure is in the matrix
+            if (rowOfTreasure == -1) {
+                printf("Parent: No treasure was found in the matrix\n"); // If no treasure was found, print and end program
+                exit(0);     
+            }                           
+            // Find column of treasure given winning row
+            for (int j = 0; j < 1000; j++) { 
                 if (matrix[rowOfTreasure][j] == 1) {
                     printf("Parent: the treasure was found by child with PID %d at row %d and column %d\n", winnerPID, rowOfTreasure, j);
                     exit(0);
-                }
+                } 
             }
-            printf("Parent: No treasure was found in the matrix."); // If no treasure was found, print and end program
-            exit(0);
         }
     } else {
-
         /* Error checking for incorrect input */
         printf("Please use the following format: ./a1p1 < [text_file]\n");
-        printf("%d\n", argc);
+        printf("Number of Arguments: %d\n", argc-1);
         for (int i = 0; i < argc; i++) {
             printf("Argument %d: %s\n", i, argv[i]);
         }
